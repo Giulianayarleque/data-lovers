f352a0f7dff0c5f744bd676724a98bb568886b0c
Giuliana <giuliana.yarlequech@gmail.com>
2021-03-02 07:46:28 -0500

    Actualizando repositorio

--- src/data.js
+++ src/data.js
@@ -1,9 +1,74 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const filterPokemonsByType = (typePokemon, dataPokemon) => {
+  if (typePokemon.value === "all") {
+    return dataPokemon;
+  } else {
+    // array funtion filter =>
+    const dataType = dataPokemon.filter((poke) => {
+      return poke.type.includes(typePokemon.value);
+    });
+    return dataType;
+  }
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const filterPokemonByWeakness = (valueWeakness, dataPokemon) => {
+  if(valueWeakness.value === "all"){
+    return dataPokemon
+  }else{
+    const dataWeakness = dataPokemon.filter(poke => poke.weaknesses.includes(valueWeakness.value));
+  return dataWeakness
+  }
+  
 };
+export const filterPokemonByResistant = (valueResistant, dataPokemon) =>{
+  if (valueResistant.value === 'all') {
+    return dataPokemon
+  }else {
+    const dataResistant = dataPokemon.filter(poke => poke.resistant.includes(valueResistant.value))
+  return dataResistant
+  }
+}
+
+export const listTypesPokemon = (dataPokemon) =>{
+  // console.log(dataPokemon);
+  const arrayTypePokemon = []
+  for(let i=0; i<dataPokemon.length; i++ ){
+    const newData = dataPokemon[i].type
+    for (let j = 0; j < newData.length; j++) {
+      // console.log(newData[j]);
+      arrayTypePokemon.push(newData[j])
+    }
+  }
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+  // constructor set => Use to remove duplicate elements from the array
+  // sort => Metodo ordena los elementos de un array (corresponde a la posicion del string de acuerdo a su valor)
+  // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  const arrayListTypePokemon = [...new Set(arrayTypePokemon)].sort()
+  // console.log(arrayListTypePokemon);
+  return arrayListTypePokemon
+}
+
+export const  listWeaknessPokemon = (dataPokemon) =>{
+  // console.log(dataPokemon);
+  const arrayWeaknessPokemon = []
+  dataPokemon.map((poke)=>{
+    const newData = poke.weaknesses
+    newData.map(weak => arrayWeaknessPokemon.push(weak)
+    )
+  })
+  const arrayListWeaknessPokemon = [...new Set(arrayWeaknessPokemon)].sort()
+  // console.log(arrayListWeaknessPokemon);
+  return arrayListWeaknessPokemon
+}
+
+export const listResistantPokemon = (dataPokemon) => {
+  // console.log(dataPokemon);
+  const arrayResistantPokemon = []
+  dataPokemon.map((poke)=>{
+    const newData = poke.resistant
+    newData.map(resis => arrayResistantPokemon.push(resis))
+  })
+  // console.log(arrayResistantPokemon);
+  const arrayListResistantPokemon = [...new Set(arrayResistantPokemon)].sort()
+  // console.log(arrayListResistantPokemon);
+  return arrayListResistantPokemon
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,164 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {
+  filterPokemonsByType,
+  filterPokemonByWeakness,
+  filterPokemonByResistant,
+  listTypesPokemon,
+  listWeaknessPokemon,
+  listResistantPokemon,
+} from "./data.js";
+import data from "./data/pokemon/pokemon.js";
 
-console.log(example, data);
+// Selectores
+const pokemonList = document.querySelector("#pokemonList");
+const typePokemon = document.querySelector(".typePokemon");
+const previousButton = document.querySelector("#previousButton");
+const nextButton = document.querySelector("#nextButton");
+// Solo para pruebas
+const fiterType = document.querySelector("#filterType");
+const filterWeaknesses = document.querySelector("#filterWeaknesses");
+const filterResistant = document.querySelector("#filterResistant");
+
+// Get pokemons
+const getPokemons = data.pokemon;
+// Variables Globales
+let dataPoke = getPokemons;
+let cp1 = 0;
+let cp2 = 9;
+
+// funcion cantida de tipos
+listTypesPokemon(dataPoke);
+listWeaknessPokemon(dataPoke);
+listResistantPokemon(dataPoke);
+
+// Funciones de paginacion
+const nextPage = (dataPokemon) => {
+  cp1 += 9;
+  cp2 += 9;
+  showPokemons(dataPokemon.slice(cp1, cp2));
+};
+
+const previousPage = (data) => {
+  cp1 -= 9;
+  cp2 -= 9;
+  showPokemons(data.slice(cp1, cp2));
+};
+
+// Funcion de eventos
+const nextNavigationPage = (data) => {
+  nextPage(data);
+};
+
+const previousNavigationPage = (data) => {
+  previousPage(data);
+};
+
+const filterPokemons = (e) => {
+  console.log(e.target.selectedIndex);
+  // console.log(e.target.length);
+  debugger;
+  if (e.target.selectedIndex < 19) {
+    console.log(filterType.label);
+    console.log(typePokemon.value);
+    dataPoke = filterPokemonsByType(typePokemon, getPokemons);
+  } else if (e.target.selectedIndex < 36) {
+    console.log(filterWeaknesses.label);
+    console.log(typePokemon.value);
+    dataPoke = filterPokemonByWeakness(typePokemon, getPokemons);
+  } else if (e.target.selectedIndex < 54) {
+    console.log(filterResistant.label);
+    console.log(typePokemon.value);
+    dataPoke = filterPokemonByResistant(typePokemon, getPokemons);
+  } else {
+    console.log("error");
+  }
+  console.log(dataPoke);
+
+  // dataPoke = filterPokemonsByType(typePokemon, getPokemons);
+  // dataPoke = filterPokemonByWeakness(typePokemon, getPokemons);
+
+  // dataPoke = filterPokemonByResistant(typePokemon, getPokemons);
+  // console.log(dataPoke);
+
+  nextPage(dataPoke);
+  previousPage(dataPoke);
+};
+
+const showPokemons = (pokemonData) => {
+  // innerHTML = '' => limpiar pokemonList
+  pokemonList.innerHTML = "";
+  pokemonData.forEach((poke) => {
+    let div = document.createElement("div");
+    div.classList.add("right__list-ul");
+    div.innerHTML = `
+              <ul class="right__ul">
+                <li class="right__ul-li pokemon" >
+                  <p>#${poke.num}</p>
+                  <img src=${poke.img} alt="">
+                  <span>${poke.name}</span>
+                </li>
+              </ul>
+    `;
+    pokemonList.append(div);
+  });
+
+  const ulPokemon = document.querySelectorAll("ul");
+  ulPokemon.forEach((e) => {
+    e.addEventListener("click", () => {
+      console.log(`Hola pokemon`);
+    });
+  });
+};
+
+// ver los primeros 9 pokemos => inicializar
+showPokemons(dataPoke.slice(cp1, cp2));
+
+// Event Change
+typePokemon.addEventListener("change", filterPokemons);
+
+// event of pagination
+nextButton.addEventListener("click", () => nextNavigationPage(dataPoke));
+previousButton.addEventListener("click", () =>
+  previousNavigationPage(dataPoke)
+);
+
+//función de Sort
+const sortUp = (dataUp) => {
+  /*dataUp.sort(function (a, b) {
+    if (a.num > b.num) {
+      return 1;
+    }
+    if (a.num < b.num) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
+  return dataUp*/
+ //console.log('Click aquí')
+ console.log(dataUp)
+}
+  //downUp(dataInput)
+ //console.log(sortUp(dataPoke))
+
+ const sortDown = (dataDown) => {
+  /*dataDown.sort(function (a, b) {
+    if (a.num > b.num) {
+      return -1;
+    }
+    if (a.num < b.num) {
+      return 1;
+    }
+    // a must be equal to b
+    return 0;
+  });
+  return dataDown*/
+  //console.log('click')
+  console.log(dataDown)
+}
+  //sortUp(dataInput)
+// console.log(sortDown(dataPoke))
+
+
+ //event of button up/down 
+upButton.addEventListener("click", () => sortUp(dataPoke));
+downButton.addEventListener("click", () => sortDown(dataPoke));
